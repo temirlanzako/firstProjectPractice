8d44fc3324598769cf8c73fe06a98f921dd594b5
Temirlan Mukashev <tim.mukashev11gmail.com>
2023-02-20 17:30:13 -0500

    4th change.

--- projects/firstPracticeProject/Main.java
+++ projects/firstPracticeProject/Main.java
@@ -1,9 +1,10 @@
-import java.sql.SQLOutput;
+
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello worldddddd !!!!!");
         System.out.println("New changes");
         System.out.println("3rd Change");
+        System.out.println("4th change");
     }
 }
